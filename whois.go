546a7245ce499446860e989fd16833f1c3c046d0
Adam Shannon <adamkshannon@gmail.com>
2016-12-16 15:12:08 -0600

    cleanup

--- whois.go
+++ whois.go
@@ -1,8 +1,12 @@
 package whois
 
 import (
+	"fmt"
+	"github.com/zonedb/zonedb"
 	"io/ioutil"
 	"net"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -11,17 +15,17 @@ const (
 	DEFAULT_TCP_TIMEOUT = time.Second * 10
 )
 
+// WhoisQuery accepts a domain and will return the full response of the whois query
+// for the given domain. If the domain is invalid or whois fails an error will
+// be returned.
 func WhoisQuery(domain string) (string, error) {
-	server, err := lookup_whois_server(domain)
-
+	server, err := lookupWhoisServer(domain)
 	if err != nil {
 		return "", err
 	}
 
 	full_address := net.JoinHostPort(server, DEFAULT_WHOIS_PORT)
-
 	conn, err := net.DialTimeout("tcp", full_address, DEFAULT_TCP_TIMEOUT)
-
 	if err != nil {
 		return "", err
 	}
@@ -30,7 +34,6 @@ func WhoisQuery(domain string) (string, error) {
 	conn.Write([]byte(payload))
 
 	buffer, err := ioutil.ReadAll(conn)
-
 	if err != nil {
 		return "", err
 	}
@@ -39,4 +42,31 @@ func WhoisQuery(domain string) (string, error) {
 
 	resp := string(buffer[:])
 	return resp, nil
+}
+
+// lookupWhoisServer digs into zonedb to find the whois server for the given
+// tld. If no server is found (or domain is invalid) an error will be returned.
+func lookupWhoisServer(domain string) (string, error) {
+	_, err := url.Parse(domain)
+	if err != nil {
+		return "", err
+	}
+
+	split := strings.Split(domain, ".")
+	// We assume there's at least a 'google' and 'com'
+	if len(split) < 2 {
+		return "", fmt.Errorf("Unable to parse domain (and find tld) for %s (split = %s)", domain, split)
+	}
+
+	zone := zonedb.PublicZone(domain)
+	if zone == nil {
+		return "", fmt.Errorf("unable to find zone for domain = %s", domain)
+	}
+
+	host := zone.WhoisServer()
+	if host != "" {
+		return host, nil
+	}
+
+	return "", fmt.Errorf("error finding whois server (empty host) for domain %s", domain)
 }
